86ea835d851d7023d3a78d63bda52430fb6b2d3f
Binhdt <binhthan13579113@gmail.com>
2022-11-30 00:48:35 +0700

    build full 2 task

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { CreateTask } from './component/create-task/create-task';
+import { TaskInformation } from './component/edit-task/edit-task';
+
+function App() {
+    return (
+        <div className="App">
+            {/* <TaskInformation /> */}
+            <CreateTask />
+        </div>
+    );
+}
+
+export default App;
